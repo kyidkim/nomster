e17e22986c76bacbf766e21cfdda7417ffcc6309
Kimberly Vandegrift <kyidmnmaiv@gmail.com>
2016-05-21 20:26:26 +0000

    photo upload feature wip

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,14 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-
+  
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
   end
-
+  
   def new
     @place = Place.new
   end
-
+  
   def create
     @place = current_user.places.create(place_params)
     if @place.valid?
@@ -17,27 +17,28 @@ class PlacesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-
+  
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @photo = Photo.new
   end
-
+  
   def edit
     @place = Place.find(params[:id])
-
+    
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
   end
-
+  
   def update
     @place = Place.find(params[:id])
-
+    
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
-
+    
     @place.update_attributes(place_params)
     if @place.valid?
       redirect_to root_path
@@ -45,19 +46,19 @@ class PlacesController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-
+  
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
-
+    
     @place.destroy
     redirect_to root_path
   end
-
+  
   private
-
+  
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
