649946e86ff23f185733fecc81523f99f358e246
Kimberly Vandegrift <kyidmnmaiv@gmail.com>
2016-05-21 23:08:59 +0000

    fixed issue with committing photos due to user_id

--- db/migrate/20160521201002_create_photos.rb
+++ db/migrate/20160521201002_create_photos.rb
@@ -1,10 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-      
       t.text :caption
       
-      t.integer :user_id
       t.integer :place_id
       
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521222857) do
+ActiveRecord::Schema.define(version: 20160521230731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160521222857) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "picture"
+    t.integer  "user_id"
   end
 
   create_table "places", force: true do |t|
