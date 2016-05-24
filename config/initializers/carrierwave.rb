c2245d2520b930001975fbf8b0c8bf320556f942
Kimberly Vandegrift <kyidmnmaiv@gmail.com>
2016-05-24 01:15:07 +0000

    finished upload to s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
-
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AKIAI6JO2ANEBZNCUSJQ"],                        # required
-    :aws_secret_access_key  => ENV["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"]                         # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
-  config.fog_directory  = ENV["nomstervandegrift"]                     # required
+  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
