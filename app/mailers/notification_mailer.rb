ea6ebd59558e159f6591c47b0c92b31dd0a0ed81
Kimberly Vandegrift <kyidmnmaiv@gmail.com>
2016-05-26 23:17:33 +0000

    troubleshooting email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
   
   def comment_added
-    mail(to: "kyidmnmaiv@gmail.com", subject: "A comment has been added to your place")
+    mail(to: "kyidmnmaiv@gmail.com", subject: "A comment has been added (hardcoded).")
   end
 end
